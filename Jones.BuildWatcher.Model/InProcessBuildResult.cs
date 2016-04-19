f10b46d150e1d7fe7597174a6ad255f2b5da3bc5
jonesopolis <davidarector@gmail.com>
2016-04-19 17:23:37 -0400

    started on grid

--- Jones.BuildWatcher.Model/InProcessBuildResult.cs
+++ Jones.BuildWatcher.Model/InProcessBuildResult.cs
@@ -4,6 +4,8 @@ namespace Jones.BuildWatcher.Model
 {
     public sealed class InProcessBuildResult : NotifyBase, IBuildResult
     {
-
+        public string ProjectName { get; set; }
+        public string BuildName { get; set; }
+        public string FriendlyName { get; set; }
     }
 }
